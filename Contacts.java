6894d6fe45be25ac1a614be90c12db05e4cd2b75
swapnil <sopiswarrinku77@gmail.com>
2022-06-01 17:17:48 -0700

    Added : Contact details for a Addressbook

---
+++ Contacts.java
@@ -0,0 +1,62 @@
+package com.Bridgelabz;
+
+public class Contacts {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String zip;
+    String state;
+    String phoneNumber;
+    String email;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public String getState() {
+        return state;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
+
