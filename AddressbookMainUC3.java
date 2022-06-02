a7282ec3f5106bf41efad15522c8fb6fe8ce06b7
swapnil <sopiswarrinku77@gmail.com>
2022-06-01 20:10:37 -0700

    Added : new feacher to Addressbook As a EditContact to edit the contact details

--- AddressbookMainUC3.java
+++ AddressbookMainUC3.java
@@ -1,2 +1,30 @@
-package com.Bridgelabz;public class AddressbookMainUC3 {
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressbookMainUC3 {
+    public static void main(String[] args) {
+
+
+        AddressbookEditContactsUC3 addressbookData = new AddressbookEditContactsUC3();
+        int choice;
+        do {
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Welcome to Address Book Program");
+            System.out.println("Kindly Enter choice \n 1. Add new Contact \n 2. Show Contact details \n 3. Edit contact Details");
+
+            choice = scan.nextInt();
+            switch (choice) {
+                case 1:
+                    addressbookData.addContacts();
+                    break;
+                case 2:
+                    addressbookData.showContacts();
+                    break;
+                case 3:
+                    addressbookData.editContacts();
+            }
+        } while (choice != 4);
+    }
 }
