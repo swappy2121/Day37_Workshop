11346a71edbc961bbab90373ae6564e3b575827f
swapnil <sopiswarrinku77@gmail.com>
2022-06-04 17:29:58 -0700

    Added : new Addressbook function for person

--- AddressbookMainUC5.java
+++ AddressbookMainUC5.java
@@ -1,2 +1,15 @@
-package com.Bridgelabz;public class AddressbookMainUC5 {
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressbookMainUC5 {
+    static Scanner sc = new Scanner(System.in);
+
+    //Using hashmap creating multiple Address book
+
+    public static void main(String[] args) {
+        AddingNewAddressbookUC5 ab = new AddingNewAddressbookUC5();
+        ab.addNewAddressBook();
+        ab.addContacts();
+    }
 }
