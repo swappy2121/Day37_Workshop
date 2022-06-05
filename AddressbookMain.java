a67292d21b92109b639df333e4b340a52728769d
swapnil <sopiswarrinku77@gmail.com>
2022-06-04 17:45:59 -0700

    Added : Function for searching person by their details from a addressbook

--- AddressbookMain.java
+++ AddressbookMain.java
@@ -1,2 +1,16 @@
-package com.Bridgelabz;public class AddressbookMain {
+package com.Bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressbookMain {
+    static Scanner sc = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        AddingFunctionForSearchPersonByDetails ab = new AddingFunctionForSearchPersonByDetails();
+        ab.addNewAddressBook();
+        ab.addContacts();
+        ab.searchByOptions();
+
+    }
 }
